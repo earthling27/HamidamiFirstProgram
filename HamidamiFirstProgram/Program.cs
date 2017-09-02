3873d073c7de8e9de9e1e5631f4cbb82d5189387
prakash aryal <weareallearthling@gmail.com>
2017-09-02 18:53:43 -0500

    Add project files.

---
+++ HamidamiFirstProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HamidamiFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
